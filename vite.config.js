fd040a64fe71aef664c538f90a11ee9bc48d4888
Alexandra Samoylo <ssamoylo46@gmail.com>
2024-12-21 18:58:52 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://<AlyaksandraS.github.io/notion",
+  base: "https://<ALexandraSamoylo.github.io/notion",
 })
